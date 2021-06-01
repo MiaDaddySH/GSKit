1907c00b9db0358f54faf3948eb51828ae8a7036
Martin <yuangang.sheng@itc-engineering.com>
2021-06-01 15:39:21 +0200

    Initial Commit

---
+++ GSKit/GSKit.h
@@ -0,0 +1,18 @@
+//
+//  GSKit.h
+//  GSKit
+//
+//  Created by Yuangang Sheng on 01.06.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GSKit.
+FOUNDATION_EXPORT double GSKitVersionNumber;
+
+//! Project version string for GSKit.
+FOUNDATION_EXPORT const unsigned char GSKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GSKit/PublicHeader.h>
+
+
